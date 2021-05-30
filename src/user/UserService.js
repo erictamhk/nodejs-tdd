37de1552b86e2c582c6b385e9f648df6017039c9
Eric <eric.m2hk@gmail.com>
2021-05-30 22:59:07 +0800

    Refactor code, using router and service to create user using async/await to replace promise then

---
+++ src/user/UserService.js
@@ -0,0 +1,16 @@
+const User = require("./User");
+const bcrypt = require("bcrypt");
+
+const save = async (body) => {
+  const hash = await bcrypt.hash(body.password, 10);
+
+  const user = {
+    username: body.username,
+    email: body.email,
+    password: hash,
+  };
+
+  await User.create(user);
+};
+
+module.exports = { save };
