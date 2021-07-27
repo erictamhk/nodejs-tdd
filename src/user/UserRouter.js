02d2ee4c2898350b163f2311aa1de4530d0ea4fb
Eric <eric.m2hk@gmail.com>
2021-07-27 22:05:26 +0800

    add basicAuthentication.js for user authentication

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require("express-validator");
 const ValidationException = require("../error/ValidationException");
 const ForbiddenException = require("../error/ForbiddenException");
 const pagination = require("../middleware/pagination");
-const bcrypt = require("bcrypt");
+const basicAuthentication = require("../middleware/basicAuthentication");
 
 router.post(
   "/api/1.0/users",
@@ -76,31 +76,14 @@ router.get("/api/1.0/users/:id", async (req, res, next) => {
   }
 });
 
-router.put("/api/1.0/users/:id", async (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (authorization) {
-    const encoded = authorization.substring(6);
-    const decoded = Buffer.from(encoded, "base64").toString("ascii");
-    const [email, password] = decoded.split(":");
-    const user = await UserService.findByEmail(email);
-    if (!user) {
-      return next(new ForbiddenException("unauthroized_user_update"));
-    }
-    // eslint-disable-next-line eqeqeq
-    if (user.id != req.params.id) {
-      return next(new ForbiddenException("unauthroized_user_update"));
-    }
-    if (user.inactive) {
-      return next(new ForbiddenException("unauthroized_user_update"));
-    }
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return next(new ForbiddenException("unauthroized_user_update"));
-    }
-    await UserService.updateUser(req.params.id, req.body);
-    return res.send();
+router.put("/api/1.0/users/:id", basicAuthentication, async (req, res, next) => {
+  const authenticatedUser = req.authenticatedUser;
+  if (!authenticatedUser || authenticatedUser.id != req.params.id) {
+    return next(new ForbiddenException("unauthroized_user_update"));
   }
-  return next(new ForbiddenException("unauthroized_user_update"));
+
+  await UserService.updateUser(req.params.id, req.body);
+  return res.send();
 });
 
 module.exports = router;
