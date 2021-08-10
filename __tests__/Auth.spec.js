1f7a1e6bb9014260fbb5cf16470e4f46c8e741ec
Eric <eric.m2hk@gmail.com>
2021-08-10 18:01:54 +0800

    testing returns token in response body when credentials are correct

--- __tests__/Auth.spec.js
+++ __tests__/Auth.spec.js
@@ -41,12 +41,12 @@ describe("Authentication", () => {
     const response = await postAuthentication({ email: "user1@mail.com", password: "P4ssword" });
     expect(response.status).toBe(200);
   });
-  it("returns only user id and username when login success", async () => {
+  it("returns only user id, username and token when login success", async () => {
     const user = await addUser();
     const response = await postAuthentication({ email: "user1@mail.com", password: "P4ssword" });
     expect(response.body.id).toBe(user.id);
     expect(response.body.username).toBe(user.username);
-    expect(Object.keys(response.body)).toEqual(["id", "username"]);
+    expect(Object.keys(response.body)).toEqual(["id", "username", "token"]);
   });
   it("returns 401 when credentials are correct", async () => {
     const response = await postAuthentication({ email: "user1@mail.com", password: "P4ssword" });
@@ -107,5 +107,10 @@ describe("Authentication", () => {
     await addUser();
     const response = await postAuthentication({ email: "user1@mail.com" });
     expect(response.status).toBe(401);
+  });
+  it("returns token in response body when credentials are correct", async () => {
+    await addUser();
+    const response = await postAuthentication({ email: "user1@mail.com", password: "P4ssword" });
+    expect(response.body.token).not.toBeUndefined();
   });
 });
