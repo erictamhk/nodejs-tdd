122838ab5659f9ebb8fd9fa7ae83e4068340c9d2
Eric <eric.m2hk@gmail.com>
2021-09-04 16:28:33 +0800

    saving hoax to database

--- __tests__/HoaxSubmit.spec.js
+++ __tests__/HoaxSubmit.spec.js
@@ -5,6 +5,7 @@ const hk = require("../locales/hk/translation.json");
 const User = require("../src/user/User");
 const sequelize = require("../src/config/database");
 const bcrypt = require("bcrypt");
+const Hoax = require("../src/hoax/Hoax");
 
 beforeAll(async () => {
   if (process.env.NODE_ENV === "test") {
@@ -12,8 +13,9 @@ beforeAll(async () => {
   }
 });
 
-beforeEach(() => {
-  return User.destroy({ truncate: { cascade: true } });
+beforeEach(async () => {
+  await Hoax.destroy({ truncate: true });
+  await User.destroy({ truncate: { cascade: true } });
 });
 
 const activeUser = {
@@ -24,6 +26,8 @@ const activeUser = {
 };
 
 const credentials = { email: activeUser.email, password: activeUser.password };
+
+const vaildContent = "Hoax content";
 
 const addUser = async (user = { ...activeUser }) => {
   const hash = await bcrypt.hash(user.password, 10);
@@ -58,7 +62,6 @@ describe("Authentication", () => {
     const response = await postHoax();
     expect(response.status).toBe(401);
   });
-
   it.each`
     language | message
     ${"hk"}  | ${hk.unauthroized_hoax_submit}
@@ -76,7 +79,32 @@ describe("Authentication", () => {
   );
   it("returns 200 when valid hoax submitted with authorized user", async () => {
     await addUser();
-    const response = await postHoax({ content: "Hoax content" }, { auth: credentials });
+    const response = await postHoax({ content: vaildContent }, { auth: credentials });
     expect(response.status).toBe(200);
+  });
+  it("saves the hoax to database when authorized user send valid request", async () => {
+    await addUser();
+    await postHoax({ content: vaildContent }, { auth: credentials });
+    const hoaxes = await Hoax.findAll();
+    expect(hoaxes.length).toBe(1);
+  });
+  it("saves the hoax content and timestamp to database", async () => {
+    await addUser();
+    const beforeSubmit = Date.now();
+    await postHoax({ content: vaildContent }, { auth: credentials });
+    const hoaxes = await Hoax.findAll();
+    const savedHoax = hoaxes[0];
+    expect(savedHoax.content).toBe(vaildContent);
+    expect(savedHoax.timestamp).toBeGreaterThan(beforeSubmit);
+    expect(savedHoax.timestamp).toBeLessThan(Date.now());
+  });
+  it.each`
+    language | message
+    ${"hk"}  | ${hk.hoax_submit_success}
+    ${"en"}  | ${en.hoax_submit_success}
+  `("return $message to success submit when language is $language", async ({ language, message }) => {
+    await addUser();
+    const response = await postHoax({ content: vaildContent }, { language: language, auth: credentials });
+    expect(response.body.message).toBe(message);
   });
 });
--- src/hoax/HoaxRouter.js
+++ src/hoax/HoaxRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const AuthenticationException = require("../auth/AuthenticationException");
+const HoaxService = require("./HoaxService");
 
 router.post("/api/1.0/hoaxes", async (req, res, next) => {
   const authenticatedUser = req.authenticatedUser;
@@ -8,7 +9,8 @@ router.post("/api/1.0/hoaxes", async (req, res, next) => {
   if (!authenticatedUser) {
     return next(new AuthenticationException("unauthroized_hoax_submit"));
   }
-  return res.send();
+  await HoaxService.save(req.body);
+  return res.send({ message: req.t("hoax_submit_success") });
 });
 
 module.exports = router;
